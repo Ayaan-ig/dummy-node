21753d4dc7ef5e3276a57a8493acfac586ab33a3
unknown <khan19112004@gmail.com>
2024-05-08 12:54:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.json({msg:'hey you'}
+    );
+})
+app.get('/route1',(req,res)=>{
+    res.send({msg:'hey you in route1'});
+})
+
+app.listen(port,()=>{console.log(`server action on port ${port}`);})
